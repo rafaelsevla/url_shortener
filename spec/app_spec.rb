877ad5e1c615cb66583bf7e27e877ee54e98c8bf
Rafitos <rafaelsevla@gmail.com>
2026-03-17 19:55:04 -0300

    add hello world with rspec

---
+++ spec/app_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+ENV['APP_ENV'] = 'test'
+
+require_relative '../app'
+require 'rspec'
+require 'rack/test'
+
+RSpec.describe 'The HelloWorld App' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it 'says hello' do
+    get '/'
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('Hello World')
+  end
+end
